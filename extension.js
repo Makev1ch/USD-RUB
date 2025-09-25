a084c500d57bb042a9883ab9fc5e499cd05afd4f
Makevich <149294123+Makev1ch@users.noreply.github.com>
2025-09-25 16:08:16 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -4,8 +4,10 @@ import St from 'gi://St';
 import Clutter from 'gi://Clutter';
 import Soup from 'gi://Soup?version=3.0';
 import GLib from 'gi://GLib';
+import Gio from 'gi://Gio';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as ExtensionUtils from 'resource:///org/gnome/shell/misc/extensionUtils.js';
 
 export default class CurrencyExtension {
     constructor() {
@@ -13,6 +15,7 @@ export default class CurrencyExtension {
         this._label = null;
         this._session = null;
         this._timeoutId = null;
+        this._settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.usd-rub');
     }
 
     _scheduleNextUpdate(interval) {
@@ -52,11 +55,33 @@ export default class CurrencyExtension {
         }
     }
 
+    _placeIndicator() {
+        const pos = this._settings.get_string('position');
+
+        switch (pos) {
+            case 'right-of-clock': {
+                const dateMenu = Main.panel.statusArea.dateMenu;
+                Main.panel._centerBox.insert_child_above(this._indicator, dateMenu.container);
+                break;
+            }
+            case 'left-of-clock': {
+                const dateMenu = Main.panel.statusArea.dateMenu;
+                Main.panel._centerBox.insert_child_below(this._indicator, dateMenu.container);
+                break;
+            }
+            case 'left':
+                Main.panel.addToStatusArea('usd-rub-indicator', this._indicator, 0, 'left');
+                break;
+            case 'right':
+            default:
+                Main.panel.addToStatusArea('usd-rub-indicator', this._indicator, 1, 'right');
+                break;
+        }
+    }
+
     enable() {
-        // создаём кнопку-индикатор
         this._indicator = new PanelMenu.Button(0.0, 'USD-RUB Indicator', false);
 
-        // текстовое поле
         this._label = new St.Label({
             style_class: 'cPanelText',
             text: '…',
@@ -65,11 +90,17 @@ export default class CurrencyExtension {
 
         this._indicator.add_child(this._label);
 
-        // добавляем в панель
-        Main.panel.addToStatusArea('usd-rub-indicator', this._indicator, 1, 'center');
-
-        // запускаем обновление
+        this._placeIndicator();
         this._updateRate();
+
+        // слушаем изменения настроек
+        this._settingsChangedId = this._settings.connect('changed::position', () => {
+            if (this._indicator) {
+                this._indicator.destroy();
+                this._indicator = null;
+            }
+            this.enable(); // пересоздать индикатор
+        });
     }
 
     disable() {
@@ -77,13 +108,17 @@ export default class CurrencyExtension {
             this._indicator.destroy();
             this._indicator = null;
         }
-        this._label = null;
-
         if (this._timeoutId) {
             GLib.Source.remove(this._timeoutId);
             this._timeoutId = null;
         }
-
-        this._session = null;
+        if (this._session) {
+            this._session = null;
+        }
+        if (this._settingsChangedId) {
+            this._settings.disconnect(this._settingsChangedId);
+            this._settingsChangedId = null;
+        }
+        this._label = null;
     }
 }
